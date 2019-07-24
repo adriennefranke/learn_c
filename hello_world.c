f5249ac2d7d33be0e617d5e6863edb1bdf067ffd
adriennefranke <18449966+adriennefranke@users.noreply.github.com>
2019-07-23 21:35:40 -0500

    hello world example reset author

---
+++ hello_world.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+	// every C program needs a main function - this is where the code starts running
+	// a semi-colon is required at the end of each line
+	// you don't actually need to return zero
+	// the return type of this function is an integer
+
+int main()
+{
+ 	printf("hello, world!\n");
+ 	return 0;
+}
