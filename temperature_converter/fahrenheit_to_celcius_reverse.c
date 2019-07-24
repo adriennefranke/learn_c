5ad52e8aa4a358e38ff16fd99315b1293656a13f
adriennefranke <18449966+adriennefranke@users.noreply.github.com>
2019-07-24 13:00:47 -0500

    started work on fahrenheit to celcius converter in reverse order

---
+++ temperature_converter/fahrenheit_to_celcius_reverse.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	float fahr, celcius;
+	float lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	fahr = upper;
+	printf("fahr celcius\n");
+	
+}
